6cd5955f62c19afe23bee9905311f119cc0853e6
Cornellius Dagmang <rad.developer03@gmail.com>
2019-06-01 02:46:12 +0800

    Initial commit

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -1,1 +1,11 @@
-import sklearn
+from sklearn import tree
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(features, labels)
+
+# should print [1, 0] for orange and apple respectively
+print(classifier.predict([[150, 0], [120, 1]]))
+

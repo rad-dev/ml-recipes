f0a4c7d35e783e24b482dd8b18dc74c72b4da7e8
Cornellius Dagmang <rad.developer03@gmail.com>
2019-06-01 03:01:24 +0800

    Initial commit

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -1,6 +1,9 @@
 from sklearn import tree
 
+
+# first number => weight, second number is texture (1 = smooth, 0 = bumpy)
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+# 0 = apple, 1 = orange
 labels = [0, 0, 1, 1]
 
 classifier = tree.DecisionTreeClassifier()
